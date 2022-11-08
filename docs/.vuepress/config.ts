d75e12b1061d907f36712bfa495656016782a024
jinfang <58760015+topsjf@users.noreply.github.com>
2022-11-09 00:35:48 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -49,15 +49,15 @@ export default defineUserConfig({
         ["meta", {name: "apple-mobile-web-app-capable", content: "yes"}],
         ["script", {type: 'text/javascript', src: '/script/demo.js'}],
         // 添加百度统计
-        ["script",{},
-            `var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();`,
-        ],
+       // ["script",{},
+       //     `var _hmt = _hmt || [];
+    //  (function() {
+     //   var hm = document.createElement("script");
+      ///  hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
+      //  var s = document.getElementsByTagName("script")[0]; 
+     //   s.parentNode.insertBefore(hm, s);
+    //  })();`,
+    //    ],
         //["link",{rel: "stylesheet", href: "/iconfont/iconfont.css"}],
     ],
     locales: {
