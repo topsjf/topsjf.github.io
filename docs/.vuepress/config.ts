6c3efe50640821f6f6320c20c08b3f13e5fac787
jinfang <58760015+topsjf@users.noreply.github.com>
2022-11-09 09:37:50 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -39,24 +39,13 @@ export default defineUserConfig({
     },
     head: [
         ["meta", {name: "robots", content: "all"}],
-        ["meta", {name: "author", content: "notejf"}],
         ["meta", {"http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate"}],
         ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
         ["meta", {"http-equiv": "Expires", content: "0"}],
-        ["meta", {name: "keywords", content: "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发"}],
+        ["meta", {name: "keywords", content: "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发, Linux"}],
         ["meta", {name: "apple-mobile-web-app-capable", content: "yes"}],
         ["script", {type: 'text/javascript', src: '/script/demo.js'}],
-        // 添加百度统计
-       // ["script",{},
-        //    `var _hmt = _hmt || [];
-    //  (function() {
-       // var hm = document.createElement("script");
-      //  hm.src = "https://hm.baidu.com/hm.js?5dd2e8c97962d57b7b8fea1737c01743";
-      //  var s = document.getElementsByTagName("script")[0]; 
-     //   s.parentNode.insertBefore(hm, s);
-     // })();`,
-      //  ],
-        //["link",{rel: "stylesheet", href: "/iconfont/iconfont.css"}],
+        ["link",{rel: "stylesheet", href: "/iconfont/iconfont.css"}],
     ],
     locales: {
         "/": {
