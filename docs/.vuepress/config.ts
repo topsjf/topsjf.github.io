2fd2e09e96a87aea551a2f0f4db5813c066287b0
jinfang <58760015+topsjf@users.noreply.github.com>
2022-11-09 09:48:11 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -42,7 +42,7 @@ export default defineUserConfig({
         ["meta", {"http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate"}],
         ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
         ["meta", {"http-equiv": "Expires", content: "0"}],
-        ["meta", {name: "keywords", content: "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发, Linux"}],
+        ["meta", {name: "keywords", content: "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发"}],
         ["meta", {name: "apple-mobile-web-app-capable", content: "yes"}],
         ["script", {type: 'text/javascript', src: '/script/demo.js'}],
         ["link",{rel: "stylesheet", href: "/iconfont/iconfont.css"}],
