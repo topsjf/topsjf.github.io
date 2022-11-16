f5814e430cab9820ddf330264c8bcc2a724c02ca
jinfang <topjfk@163.com>
2022-11-16 08:58:35 +0000

    更新导航栏设置

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,9 +4,9 @@ export const navbarConfig = navbar([
     "/",
     "/home",
     {text: "面试宝典", link: "/all/all"},
-    {text: "升级专区", link: "/architecture/architecture"},
-    {text: "开源专区", link: "/open-source/open-source"},
-    {text: "技术书籍", link: "/books/books"},
+   // {text: "升级专区", link: "/architecture/architecture"},
+   // {text: "开源专区", link: "/open-source/open-source"},
+   // {text: "技术书籍", link: "/books/books"},
     {
         text: "个人中心",
         icon: "anonymous",
