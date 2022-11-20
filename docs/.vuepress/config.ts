3fd024f1f26cbbdeba1d2731d7b1160b2d2f74af
jinfang <topjfk@163.com>
2022-11-20 06:35:51 +0000

    添加搜索引擎

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,8 @@
 // .vuepress/config.ts
 import theme from "./theme.ts";
-import {cp, search} from "./plugins/index.ts";
-import searchPro from "./plugins/searchPro.ts";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+//import {cp, search} from "./plugins/index.ts";
+//import searchPro from "./plugins/searchPro.ts";
 import {defineUserConfig, viteBundler} from "vuepress";
 import {copyCode} from "vuepress-theme-hope";
 
@@ -18,7 +19,41 @@ export default defineUserConfig({
     },
     
     plugins: [
-        searchPro,
+      searchProPlugin({
+      // 搜索引擎，是否索引内容
+      indexContent: true,
+      // 配置选项
+      customFields: [
+        {
+          name: "title",
+          getter: (page) => page.frontmatter.title,
+          formatter: {
+            "/": "标题：$content",
+          },
+        },
+        {
+          name: "category",
+          getter: (page) => page.frontmatter.category,
+          formatter: {
+            "/": "分类：$content",
+          },
+        },
+        {
+          name: "tag",
+          getter: (page) => page.frontmatter.tag,
+          formatter: {
+            "/": "标签：$content",
+          },
+        },
+        {
+          name: "updateTime",
+          getter: (page) => page.data.git?.updateTime.toLocaleString(),
+          formatter: {
+            "/": "更新时间：$content",
+          },
+        },
+      ]
+    }),
     ],
     
     bundler: viteBundler({
