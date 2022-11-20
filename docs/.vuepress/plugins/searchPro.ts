2978a85df8a4fa3577f6ef260292c1dc4c278980
jinfang <topjfk@163.com>
2022-11-20 07:17:30 +0000

    添加搜索引擎

--- docs/.vuepress/plugins/searchPro.ts
+++ docs/.vuepress/plugins/searchPro.ts
@@ -4,36 +4,26 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 //export const searchPro = () =>
 export default searchProPlugin({
       // 搜索引擎，是否索引内容
-      indexContent: true,
-      // 配置选项
+      //indexContent: true,
       customFields: [
         {
           name: "title",
           getter: (page) => page.frontmatter.title,
-          formatter: {
-            "/": "标题：$content",
-          },
+          formatter: "标题：$content",
+         
         },
         {
           name: "category",
           getter: (page) => page.frontmatter.category,
-          formatter: {
-            "/": "分类：$content",
-          },
+          formatter: "分类：$content",
+         
         },
         {
           name: "tag",
           getter: (page) => page.frontmatter.tag,
-          formatter: {
-            "/": "标签：$content",
-          },
+          formatter: "标签：$content",
+          
         },
-        {
-          name: "updateTime",
-          getter: (page) => page.data.git?.updateTime.toLocaleString(),
-          formatter: {
-            "/": "更新时间：$content",
-          },
-        },
-      ]
-    })
+        
+      ],
+})
